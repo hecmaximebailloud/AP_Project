8a0e374a4fea426167ff000abac1e789bd006062
hecmaximebailloud <maxime.bailloud@unil.ch>
2024-05-22 10:33:54 +0200

    Update AP_Projet_code.py

--- AP_Projet_code.py
+++ AP_Projet_code.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from scripts.data_processing import preprocess_all_data, calculate_returns, calculate_volatility, fetch_latest_news
+from scripts.data_processing import preprocess_all_data, calculate_returns, calculate_volatility
 
 
 # Define start and end dates for the weekly data
